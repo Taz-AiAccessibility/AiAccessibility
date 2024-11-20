da0ad444ef489f4c68c3c203d46c5eba13a46ebe
Julie Hoagland-Sorensen <143046484+JulieHoaglandSorensen@users.noreply.github.com>
2024-11-20 15:22:31 -0500

    Dev branch created

---
+++ server/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+//import is compatible everywhere,
+// where as "require" is only usable under certain circumstances so unless you are working
+//with a legacy codebase that already uses "require" you should use "import" instead.
+//import 'dotenv/config//
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`);
+});
