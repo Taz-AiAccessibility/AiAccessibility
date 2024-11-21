310dd35f19a547ff74408ab1f3557190c5259cd9
Ragad Mohammed <r1mohamm@gmail.com>
2024-11-21 13:11:18 -0800

    style update

--- src/components/ImageContextInput.jsx
+++ src/components/ImageContextInput.jsx
@@ -7,9 +7,9 @@ const ImageContextInput = ({ imageContext, setImageContext } ) => {
 
   return (
     <div id='imageContextInput' className='inputBox'>
-      <label htmlFor='imageContext'>Image Context </label>
-      <input type='text' id='imageContext' name='imageContext' value={imageContext} onChange={handleChange}></input>
-      <p>Optional: provide additional information about the image. For example, names of people or places, dates, etc.</p>
+      <label htmlFor='imageContext'>Image Details: </label>
+      <input type='text' id='imageContext' name='imageContext' placeholder='Optional' value={imageContext} onChange={handleChange}></input>
+      <p>Provide additional information about the image. For example, names of people or places, dates, etc.</p>
     </div>
   );
 };
