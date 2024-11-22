ebc79922fbc9ad0be34ac14cae898c2ae88a5159
Ian Buchanan <get.ian.buchanan@gmail.com>
2024-11-21 16:40:50 -0800

    add basic url error handling

--- src/components/ImageURLInput.jsx
+++ src/components/ImageURLInput.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const isImageUrl = async (url) => {
+const isImageUrl = (url) => {
   const imageRegex = /(\.(png|jpe?g|gif|svg))(\?.*)?/i;
   return imageRegex.test(url);
 };
@@ -15,14 +15,13 @@ const ImageURLInput = ({
 }) => {
   const handleChange = async (event) => {
     const url = event.target.value;
-    // could add url validation
-    // if (!isImageUrl(url)) {
-    //   setIsUrlError(true);
-    //   setUrlError('not valid image url');
-    // }
-    // setIsUrlError(false);
-    // setUrlError('');
-
+    if (!isImageUrl(url)) {
+      setIsUrlError(true);
+      setUrlError('not valid image url');
+    } else {
+      setIsUrlError(false);
+      setUrlError('');
+    }
     setImageURL(url);
   };
 
@@ -37,54 +36,19 @@ const ImageURLInput = ({
         onChange={handleChange}
       ></input>
 
-      {/* {isUrlError && <p>{urlError}</p>} */}
+      {isUrlError && imageURL && (
+        <p
+          style={{
+            color: 'red',
+            fontSize: '14px',
+            fontWeight: 'bold',
+          }}
+        >
+          {urlError}
+        </p>
+      )}
     </div>
   );
 };
 
 export default ImageURLInput;
-
-// const isValidUrlWithImage = async (url) => {
-//   try {
-//     // Check if URL is valid
-//     // replace with regex
-//     //new URL(url);
-
-//     const isValid = await isImageUrl(url);
-
-//     // Fetch the URL with HEAD method to avoid downloading the content
-
-//     if (!isValid) {
-//       //update error state
-//       // error boolean
-//       setIsUrlError(true);
-//       setUrlError('url is not an image');
-//       console.log('url is not an image');
-//       return;
-//     }
-
-//     const response = await fetch(url);
-
-//     if (!response.ok) {
-//       // deal with state/css
-//       // update error state
-//       // error boolean
-//       setIsUrlError(true);
-//       setUrlError('url is not a valid path');
-//       //console.error('url is not a valid path');
-//       return;
-//     }
-
-//     setIsUrlError(false);
-//     setUrlError('');
-//     return;
-
-//     // clear error boolean and state
-//   } catch (error) {
-//     // setIsUrlError(true);
-//     // setUrlError('url is not a valid path');
-//     // console.error('url is not a valid path');
-//     console.error('Invalid URL or inaccessible resource:', error.message);
-//     return;
-//   }
-// }
